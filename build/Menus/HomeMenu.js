4e8a285a48826ad8974153ca20c2d94deca1ba47
weslley matos <weslley_mattosibf@hotmail.com>
2022-09-23 08:11:16 -0300

    I finished a new version

---
+++ build/Menus/HomeMenu.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleShowHomeMenu = void 0;
+function handleShowHomeMenu() {
+}
+exports.handleShowHomeMenu = handleShowHomeMenu;
---
+++ build/Menus/ProfileMenu.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleShowProfileMenu = void 0;
+function handleShowProfileMenu() {
+    return '';
+}
+exports.handleShowProfileMenu = handleShowProfileMenu;
